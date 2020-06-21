f2a5633d813d17eddadb5b1c94ef649e84251412
LYNN DAMOTA <bdamota0506@gmail.com>
2020-06-21 00:54:43 -0700

    refactored html and additional css

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,13 @@ var time = moment();
 // Function to set and enter text into the grid:
 function setPlanner() {
     // Date function from moment.js:
-    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
     // To retrieve the schedule-field text from localStorage:
-    $(".time-block").each(function () {
+    $(".form-group").each(function () {
         var id = $(this).attr("id");
-        var schedule = localStorage.getItem(id);
+        var schedule = localStorage.getItem(schedule);
 
         if (schedule !== null) {
             $(this).children(".schedule").val(schedule);
@@ -20,12 +20,17 @@ setPlanner();
 
 // Saves to localStorage:
 var saveBtn = $(".saveBtn");
+    
 
 saveBtn.on("click", function () {
     var time = $(this).parent().attr("id");
-    var schedule = $(this).siblings(".schedule").val();
+    var schedule = $(this).siblings(".schedule").val;
 
-    localStorage.setItem(time, schedule);
+    localStorage.setItem("time", JSON.stringify(time));
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+
+    localStorage.getItem("time");
+    localStorage.getItem("schedule");
 });
 
 // Function for changing from past to present to future on the grid:

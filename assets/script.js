fc5fdc514e3fcc2b31b25992117a6435e292f5dd
LYNN DAMOTA <bdamota0506@gmail.com>
2020-06-20 22:27:12 -0700

    initial jquery for save to local storage

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,34 @@ function setPlanner() {
 }
 setPlanner();
 
+// Saves to localStorage:
+var saveBtn = $(".saveBtn");
 
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
+
+// Function for changing from past to present to future on the grid:
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+pastPresentFuture();
 
   
